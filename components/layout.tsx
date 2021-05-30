7a571314a6c175b2325804bf95a37c09c686b794
Matthew Wen <mattwen2018@gmail.com>
2021-05-29 22:17:32 -0500

    Added New Nav Bar

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Link from 'next/link'
 import Copyright from "../src/Copyright"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Nav, Navbar, NavDropdown} from "react-bootstrap";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -12,31 +14,37 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-function NavBar() {
+function ECESSNavBar() {
     const nav_links = [['/', 'Home'], ['/calendar', 'Calendar'], ['/fun', 'Fun'],
         ['/projects', 'Projects'], ['/about', 'About']];
     return (
-        <div style={{border: "solid"}}>
-            <p>Temporary Navigation Bar</p>
-            <ol>
-                {nav_links.map((item, i) => {
-                    return (
-                        <li key={item[0]}>
-                            <Link href={item[0]}>{item[1]}</Link>
-                        </li>
-                    )
-                })}
-            </ol>
-        </div>
+        <Navbar bg="light" expand="lg">
+            <Link href={"/"} passHref>
+                <Navbar.Brand>Purdue ECE Ambassadors</Navbar.Brand>
+            </Link>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    {nav_links.map((item, i) => {
+                        return (
+                            <Link key={item[0]} href={item[0]} passHref>
+                                <Nav.Link >
+                                    {item[1]}
+                                </Nav.Link>
+                            </Link>
+                        )
+                    })}
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
 
 export default function Layout({children}) {
-    const classes = useStyles();
     return (
         <div style={{margin: 0, height: "100vh", display: "flex", flexDirection: "column"}}>
             <div style={{flex: 1}}>
-                <NavBar/>
+                <ECESSNavBar/>
                 <main style={{margin: "0 auto", maxWidth: 1080}}>
                     <div style={{margin: 5}}>
                         {children}
