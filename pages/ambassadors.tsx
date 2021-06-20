e56bf653e01c204abfba25ebb347900c7306c3b7
jin221 <70247451+jin221@users.noreply.github.com>
2021-06-20 02:04:32 -0400

    Amb info (#9)
    * ambassadors info #1
    
    * Ambassadors info
    
    * Fix Alignment and Added Myself
    
    Co-authored-by: root <root@LAPTOP-D2PRR2JN.localdomain>
    Co-authored-by: Matthew Wen <mattwen2018@gmail.com>

--- pages/ambassadors.tsx
+++ pages/ambassadors.tsx
@@ -4,32 +4,43 @@ import Head from "next/head";
 import AMBASSADORS from "../data/data_people";
 import {Button, Card, CardActions, CardContent, Typography} from "@material-ui/core";
 
+import { useState, useEffect } from 'react';
 interface Props {
     key: any,
     window?: () => Window;
 }
 
 function AmbassadorView(props) {
-    const {person} = props;
+    const {person, width} = props;
+    const setWidth = width ? width: 350;
     return (
-        <Card style={{background: '#fff', display: 'inline-block', margin: 10}}>
-            <CardContent>
+        <Card style={{background: '#E0EEEA', display: 'inline-block', margin: 5}}>
+            <CardContent style={{width: setWidth, overflow: 'hidden'}}>
                 <div style={{display: 'flex'}}>
-                    <div style={{maxHeight: 200, maxWidth: 200, overflow: 'hidden'}}>
-                        <img src={'/ecea' + person.picture} alt={'student profile'} height={200}/>
+                    <div style={{maxHeight: 200, maxWidth: 150, overflow: 'hidden'}}>
+                        <div style={{display: 'flex', justifyContent: 'center'}}>
+                            <img src={'/ecea' + person.picture} alt={'student profile'} height={200}/>
+                        </div>
                     </div>
                     <div style={{flex: 1, marginLeft: 10}}>
                         <Typography variant={'h6'}>
                             {person.name}
                         </Typography>
+                        {person.major ?
+                            <Typography variant={"subtitle2"}>
+                                {person.major}
+                            </Typography> : <></>
+                        }
                         {person.graduation ?
                             <Typography variant={"subtitle2"}>
                                 {person.graduation}
                             </Typography> : <></>
                         }
                         {person.quote ?
-                            <Typography variant={"body1"}>
-                                {person.quote}
+                            <Typography variant={"body2"} style={{fontStyle: 'italic', marginTop: 10}}>
+                                <q>
+                                    {person.quote}
+                                </q>
                             </Typography> : <></>
                         }
                     </div>
@@ -50,25 +61,64 @@ function AmbassadorView(props) {
     );
 }
 
+function useWindowSize() {
+    const [windowSize, setWindowSize] = useState({
+        width: undefined
+    });
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const handleResize = () => {
+                const width = window.innerWidth;
+                setWindowSize({
+                    width: getAmbassadorWidth(width > 1080 ? 1080: width)
+                });
+            }
+            window.addEventListener("resize", handleResize);
+            handleResize();
+            return () => window.removeEventListener("resize", handleResize);
+        }
+    }, []);
+    return windowSize;
+}
+
+function getAmbassadorWidth(screenWidth) {
+    const minWidth = 350;
+    let numPeople = Math.floor(screenWidth / minWidth);
+    let ambassadorWidth = screenWidth;
+    if (numPeople > 1) {
+        ambassadorWidth /= numPeople;
+    }
+    else {
+        numPeople = 2.8;
+    }
+    return Math.floor(ambassadorWidth) - Math.floor(10 * numPeople);
+}
 
 export default function AboutPage(props: Props) {
+    const size = useWindowSize();
     return (
-        <Layout maxWidth={undefined}>
+        <Layout maxWidth={1080}>
             <>
                 <Head>
                     <title>ECEA: About</title>
                 </Head>
 
-                <div style={{textAlign: 'center'}}>
-                    {AMBASSADORS.map((item, i) => {
-                        return (
-                            <AmbassadorView key={i} person={item}/>
-                        )
-                    })}
-
-                </div>
+                {
+                    size.width ?
+                        <div style={{margin: '0 auto', fontSize: 0}}>
+                            {AMBASSADORS.map((item, i) => {
+                                return (
+                                    <AmbassadorView key={i} person={item} width={size.width}/>
+                                )
+                            })}
+                        </div>
+                        : <></>
+                }
             </>
         </Layout>
     )
 }
+
+
 
