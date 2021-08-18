2e6fff541b419da1f691cf2203bf773344efecab
Matthew Wen <mattwen2018@gmail.com>
2021-08-18 10:17:42 -0500

    Moved Pictures, Updated Fun Page

---
+++ src/data/data_fun.tsx
@@ -0,0 +1,54 @@
+export const FUN = [
+    {
+        'title': 'Where are they now Summer 2021',
+        'date': 'May 25, 2021 to August 21, 2021',
+        'description':
+            `Where are they now takes a closer look into some of the Ambassador's summer plans for 2021. We discuss some
+            of their backgrounds (hometown, hobbies), which companies they were working at (TTM, Sandia National Labs, 
+            BP, Cummins, or Amazon), any other fun summer plans, and 1 piece of advice to our fellow ECE friends. These
+            are some of the Ambassadors we showcased. <br/><br/>
+            Order (Left to Right, Up to Down): Sara Hui, Amita Pandey, Sam Dlott, Alyssa DeLouise, Michael Worthington, 
+            Denae Galloway, Sihun Kim, Brady Smith, Matthew Wen
+            `,
+        'photos': [
+            {'ref': '8-18-2021-where-are-they-now/sara_hui.jpg', col: 1},
+            {'ref': '8-18-2021-where-are-they-now/amita_pandey.jpg', col: 3},
+            {'ref': '8-18-2021-where-are-they-now/sam_dlott.jpg', col: 2},
+            {'ref': '8-18-2021-where-are-they-now/alyssa_delouise.jpg', col: 2},
+            {'ref': '8-18-2021-where-are-they-now/mike_worthington.jpg', col: 2},
+            {'ref': '8-18-2021-where-are-they-now/denae_galloway.jpg', col: 1},
+            {'ref': '8-18-2021-where-are-they-now/sihun_kim.jpg', col: 1},
+            {'ref': '8-18-2021-where-are-they-now/brady_smith.jpg', col: 2},
+            {'ref': '8-18-2021-where-are-they-now/matthew_wen.jpg', col: 2},
+        ]
+    },
+    {
+        'title': 'ECEA Headshots and Pictures Event',
+        'date': 'April 16, 2021',
+        'description':
+            `The Ambassadors here decided to get Headshots for their Linkedin etc. In addition, we collected some fun group 
+            photos here as a group. Here are a collection of some of those photos. Hope you enjoy!!
+            `,
+        'maxHeight': 500,
+        'photos': [
+            {'ref': '4-16-2021-headshots/8c156a5166704ec4996c204db2ad0919.jpg'},
+            {'ref': '4-16-2021-headshots/c521897d1c564271cf8157b15d97c2ca.jpg'},
+            {'ref': '4-16-2021-headshots/d8a55710b4e2a28bef61eeaadc961a74.jpg'},
+            {'ref': '4-16-2021-headshots/db3e530d701893f51938d545ac0fb6de.jpg'},
+            {'ref': '4-16-2021-headshots/0bf270e223e98350b6770f4d1893e2f7.jpg', col: 2},
+            {'ref': '4-16-2021-headshots/6a656652329d2e88aa19b9b164de27d2.jpg', col: 2},
+            {'ref': '4-16-2021-headshots/bec6f0aea811a44340b873cc7ce57e77.jpg', col: 2},
+            {'ref': '4-16-2021-headshots/e7282ff2de26dd6e5176b2e6ca44d6b8.jpg', col: 2},
+            {'ref': '4-16-2021-headshots/1f20bbc7d9baad58653ad6b1ec2b43a9.jpg', col: 2},
+            {'ref': '4-16-2021-headshots/7db2a11d9389595c9f51c7e8a97668b0.jpg', col: 2},
+            {'ref': '4-16-2021-headshots/f54caa40ea454855e8f6a0448173f63f.jpg', col: 2},
+            {'ref': '4-16-2021-headshots/26fa08f9c36cb813d8778bb19f83a7ae.jpg', col: 2},
+            {'ref': '4-16-2021-headshots/cc7c44ff3d31623625564f403e188283.jpg', col: 2},
+            {'ref': '4-16-2021-headshots/f915717ee4d52f70f92c2590efeee44f.jpg', col: 2},
+            {'ref': '4-16-2021-headshots/447cc5c569de3ac912d4bb408df4af65.jpg', col: 2},
+            {'ref': '4-16-2021-headshots/96d464123fa16923deecee14f22ee333.jpg', col: 2},
+            {'ref': '4-16-2021-headshots/4d5b0e835542d04b1615a6cec95aa1f8.jpg', col: 2},
+            {'ref': '4-16-2021-headshots/a608146ee287fc344b5e734abcdb4fd7.jpg', col: 2}
+        ]
+    }
+]
