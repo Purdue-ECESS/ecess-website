cb4fb37af3fca3b3814e7f3e9a33e5f4fdfb7a72
Matthew Wen <mattwen2018@gmail.com>
2021-09-06 09:02:56 -0500

    Update data_posts.tsx

--- src/data/data_posts.tsx
+++ src/data/data_posts.tsx
@@ -1,11 +1,11 @@
 const PURDUE_UPDATES = [
     {
-        title: 'Fall 2021 Welcome Back Week!!',
-        content: 'Starting August 23, 2021 is Welcome Back Week for ECE, FYE, and Grad ECE Students. Check out this <a href="https://www.instagram.com/p/CSpQ9TKFvrL/?utm_source=ig_web_copy_link">instagram post</a> to see the events!!'
-    },
-    {
         title: 'Applications for Ambassadors for Fall 2021 🎉',
         content: '❗️Application to be an ambassador is due on September 10, 2021❗️ Click on <a href="https://forms.gle/MJYUa7Wm3QRBUhbD9">this link</a> to apply ☺️'
+    },
+    {
+        title: 'Fall 2021 Welcome Back Week!!',
+        content: 'Starting August 23, 2021 is Welcome Back Week for ECE, FYE, and Grad ECE Students. Check out this <a href="https://www.instagram.com/p/CSpQ9TKFvrL/?utm_source=ig_web_copy_link">instagram post</a> to see the events!!'
     }
 ]
 export default PURDUE_UPDATES;
