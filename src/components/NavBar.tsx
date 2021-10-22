45c78ff73363011bd69938dd43b0acc7ec0ec940
Matthew Wen <mattwen2018@gmail.com>
2021-10-22 19:53:11 -0400

    done

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,11 +6,37 @@ import { Link } from "react-router-dom";
 import {Typography} from "@material-ui/core";
 import "../styles/navbar.css";
 
+
 export function NavBar(props: any) {
-    const {navLinks} = props;
-    const [expand, updateExpanded] = useState(false);
     const [navColour, updateNavbar] = useState(false);
     const [linkIdx, setLinkIdx] = useState(-1);
+    const [expand, updateExpanded] = useState(false);
+    const [title, setTitle] = useState("Ambassadors");
+
+    const setECESSPage = () => {
+        setTitle(undefined);
+        setNavLinks(ECESS_NAV_LINKS);
+    }
+    const setAmbassadorPage = () => {
+        setTitle('Ambassadors');
+        setNavLinks(AMBASSADOR_NAV_LINKS);
+    }
+    const AMBASSADOR_NAV_LINKS =  [
+        {link: '/calendar', label: 'Calendar', onClick:  setAmbassadorPage},
+        {link: '/fun', label: 'Fun', onClick:  setAmbassadorPage},
+        {link: '/ece', label: 'ECE', onClick:  setAmbassadorPage},
+        {link: '/members', label: 'Members', onClick:  setAmbassadorPage},
+        // {link: '/temp', label: 'Temp', onClick:  setECESSPage}
+    ];
+    const ECESS_NAV_LINKS = [
+        {link: '/board', label: 'Board', onClick:  setECESSPage},
+        {link: '/committees', label: 'Committees', onClick:  setECESSPage},
+        {link: '/', label: 'Ambassadors', onClick:  () => {
+            setAmbassadorPage();
+            setLinkIdx(0);
+            }}
+    ]
+    const [navLinks, setNavLinks] = useState(AMBASSADOR_NAV_LINKS)
 
     function scrollHandler() {
         if (window.scrollY >= 20) {
@@ -42,35 +68,38 @@ export function NavBar(props: any) {
                 </Navbar.Toggle>
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ml-auto" defaultActiveKey="#home">
-                        <Nav.Item>
-                            <Nav.Link
-                                className="hover-underline-animation"
-                                as={Link}
-                                to="/"
-                                onClick={() => {
-                                    setLinkIdx(-1);
-                                    updateExpanded(false)
-                                }}
-                            >
-                                <Typography style={{fontWeight: (linkIdx === -1 ? "bold": undefined)}}>
-                                    Ambassadors
-                                </Typography>
-                            </Nav.Link>
-                        </Nav.Item>
-
-                        {navLinks ? navLinks.map((i, idx) => (
+                        {title ?
                             <Nav.Item>
                                 <Nav.Link
                                     className="hover-underline-animation"
                                     as={Link}
-                                    to={i[0]}
+                                    to="/"
                                     onClick={() => {
-                                        setLinkIdx(idx);
+                                        setLinkIdx(-1);
                                         updateExpanded(false)
                                     }}
                                 >
+                                    <Typography style={{fontWeight: (linkIdx === -1 ? "bold": undefined)}}>
+                                        {title}
+                                    </Typography>
+                                </Nav.Link>
+                            </Nav.Item>: <></>
+                        }
+
+                        {navLinks ? navLinks.map((i, idx) => (
+                            <Nav.Item key={i.link}>
+                                <Nav.Link
+                                    className="hover-underline-animation"
+                                    as={Link}
+                                    to={i.link}
+                                    onClick={() => {
+                                        setLinkIdx(idx);
+                                        i.onClick();
+                                        updateExpanded(false);
+                                    }}
+                                >
                                     <Typography style={{fontWeight: (linkIdx === idx ? "bold": undefined)}}>
-                                        {i[1]}
+                                        {i.label}
                                     </Typography>
                                 </Nav.Link>
                             </Nav.Item>
