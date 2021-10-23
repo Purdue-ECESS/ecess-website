9673e3f4056478e93595fe1c74af84df43b687f6
Matthew Wen <mattwen2018@gmail.com>
2021-10-23 11:58:03 -0400

    nav bar for merger should be fixed (#55)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,27 +1,32 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
+import { useHistory } from 'react-router-dom'
 import {Typography} from "@material-ui/core";
 import "../styles/navbar.css";
 import { useLocation } from 'react-router-dom';
+
+const getLinkIdxByPathName = (location, navLinks) => {
+    for (let i = 0; navLinks && i < navLinks.length; i++) {
+        if (navLinks[i].link === location) {
+            return i;
+        }
+    }
+    return -1;
+}
 
 export function NavBar() {
     const [root, setRoot] = useState('/');
     const [navColour, updateNavbar] = useState(false);
     const [expand, updateExpanded] = useState(false);
     const location = useLocation().pathname;
+    const history = useHistory();
 
-    const [title, setTitle] = useState(location.startsWith('/ecess') ? undefined : (location.startsWith('/wece') ? 'Women in ECE': 'Ambassadors'));
-    const getLinkIdxByPathName = (location) => {
-        for (let i = 0; navLinks && i < navLinks.length; i++) {
-            if (navLinks[i].link === location) {
-                return i;
-            }
-        }
-        return -1;
-    }
+    const getTitle = (x) => x.startsWith('/ecess') ? undefined : (x.startsWith('/wece') ? 'Women in ECE': 'Ambassadors');
+
+    const [title, setTitle] = useState(getTitle(location));
     const setECESSPage = () => {
         setTitle(undefined);
         setNavLinks(ECESS_NAV_LINKS);
@@ -60,11 +65,10 @@ export function NavBar() {
         {link: '/wece/members', label: 'Members', onClick: setWECEPage},
         {link: '/wece/resources', label: 'Resources', onClick: setWECEPage}
     ]
-    const [navLinks, setNavLinks] = useState(location.startsWith('/ecess') ? ECESS_NAV_LINKS : (location.startsWith('/wece') ? WECE_NAV_LINKS: AMBASSADOR_NAV_LINKS));
+    const getNavLinks = (x) => x.startsWith('/ecess') ? ECESS_NAV_LINKS : (x.startsWith('/wece') ? WECE_NAV_LINKS: AMBASSADOR_NAV_LINKS);
+    const [navLinks, setNavLinks] = useState(getNavLinks(location));
 
-
-    const [linkIdx, setLinkIdx] = useState(getLinkIdxByPathName(location));
-
+    const [linkIdx, setLinkIdx] = useState(getLinkIdxByPathName(location, navLinks));
 
     function scrollHandler() {
         if (window.scrollY >= 20) {
@@ -75,6 +79,16 @@ export function NavBar() {
     }
 
     window.addEventListener("scroll", scrollHandler);
+
+    useEffect(() => {
+        return history.listen(location => {
+            const pathname = location.pathname;
+            const navLinks = getNavLinks(pathname);
+            setTitle(getTitle(pathname));
+            setNavLinks(getNavLinks(pathname));
+            setLinkIdx(getLinkIdxByPathName(pathname, navLinks));
+        })
+    }, [getNavLinks, history])
 
     return (
         <Navbar
