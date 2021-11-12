79ef5ef90819014cf04f4ccad93c5a0fddb449ad
Matthew Wen <mattwen2018@gmail.com>
2021-11-12 11:44:02 -0500

    nav toggle is now all white (#72)

--- src/pages/ecea/ecea_members.tsx
+++ src/pages/ecea/ecea_members.tsx
@@ -3,7 +3,6 @@ import {getMembersFromOrganization} from "../../data/data_people";
 import {MemberList} from "../../components/member_list";
 import {Chip, Typography} from "@material-ui/core";
 import {hashCode, intToRGB} from "../../utils";
-import {DarkTypography} from "../../components/dark_typography";
 
 
 export default function AboutPage() {
