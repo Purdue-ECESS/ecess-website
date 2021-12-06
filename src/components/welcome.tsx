ca96ae08588317a6da2606352d48c944634c8455
Matthew Wen <mattwen2018@gmail.com>
2021-12-06 01:56:43 -0500

    revert background img from div to img (#77)

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -1,35 +1,36 @@
 import React from "react";
-import {ImageLoader} from "./image_loader";
 
 export function WelcomeImage(props: any) {
     const {picture, children} = props;
     return (
         <div style={{display: 'block',}} className={"dark"}>
-            <ImageLoader
-                style={{
-                    borderRadius: "0px 0px 25px 25px",
-                    overflow: 'hidden', maxHeight: 450, minHeight: 50,
-                }}
-                height={450}
-                width={"100%"}
-                src={picture}/>
+            <div style={{
+                borderRadius: "0px 0px 25px 25px",
+                width: "100%", overflow: 'hidden', maxHeight: 450, minHeight: 50}}>
+                <img
+                    style={{
+                        position: 'relative', width: "100%", minWidth: "500px", marginTop: "-10%"}}
+                    src={picture}
+                    alt={"Welcome Page"} />
+            </div>
+
             {
-                children ?
-                    <div style={{
-                        position: 'relative',
-                        marginTop: "-50px",
-                        width: "90%",
-                        marginLeft: 'auto',
-                        marginRight: 'auto',
-                        textAlign: 'center', zIndex: 100}}>
-                        <div
-                            style={{display: 'inline-block',
-                                borderRadius: '25px',
-                                padding: 5,
-                                backgroundColor: '#CEB888', maxWidth: 550, margin: '0 auto'}}>
-                            {children}
-                        </div>
-                    </div> : <></>
+                children &&
+                <div style={{
+                    position: 'relative',
+                    marginTop: "-50px",
+                    width: "90%",
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                    textAlign: 'center', zIndex: 100}}>
+                    <div
+                        style={{display: 'inline-block',
+                            borderRadius: '25px',
+                            padding: 5,
+                            backgroundColor: '#CEB888', maxWidth: 550, margin: '0 auto'}}>
+                        {children}
+                    </div>
+                </div>
             }
         </div>
     );
