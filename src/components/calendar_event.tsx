aed3df6bbb9649c1828f3a3ffed6460e7367b182
Matthew Wen <mattwen2018@gmail.com>
2021-12-07 02:13:33 -0500

    updated calendar view to add more information from discord

--- src/components/calendar_event.tsx
+++ src/components/calendar_event.tsx
@@ -1,7 +1,7 @@
 import {Card, CardContent, Chip, Typography} from "@material-ui/core";
 import React from "react";
 import moment from "moment";
-
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 export default function CalendarEvent(props) {
     const {item} = props;
@@ -13,7 +13,18 @@ export default function CalendarEvent(props) {
                 <div style={{overflowX: "scroll"}}>
                     <Chip style={{backgroundColor: '#ffa0a0'}} label={startDate.format('MMM Do')}/>
                     <Typography variant={"h6"}>{item.title}</Typography>
+                    {
+                        item.location &&
+                        <div style={{display: "flex", flex: "wrap"}}>
+                            <LocationOnIcon style={{width: 15}}/>
+                            <Typography style={{marginTop: "auto", marginBottom: "auto", fontSize: 10}}>{item.location}</Typography>
+                        </div>
+                    }
                     <Typography>{startDate.format('h:mm A')} - {endDate.format('h:mm A')}</Typography>
+                    {
+                        item.content &&
+                        <Typography style={{marginTop: 1, marginBottom: 1, fontSize: 12}} variant={"body1"} dangerouslySetInnerHTML={{__html: item.content}}/>
+                    }
                 </div>
             </CardContent>
         </Card>
