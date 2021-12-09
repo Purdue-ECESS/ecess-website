bd1c80d480aa7fead14ea1c754901b0c19f79859
Matthew Wen <mattwen2018@gmail.com>
2021-12-09 06:03:52 -0500

    created spark directory each with it's own component

---
+++ src/pages/spark/spark_index.tsx
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from "react";
+
+export function SparkIndex() {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return (
+            <div
+                style={{height: "calc(100vh - 69px)", width: "100vw"}}/>
+        )
+    }
+
+    return (
+        <iframe title={"spark"}
+                loading="lazy"
+                src={"https://purdue-ecess.github.io/spark-website/"}
+                frameBorder={0}
+                style={{height: "calc(100vh - 69px)", width: "100vw"}}/>
+    )
+
+}
---
+++ src/pages/spark/spark_schedule.tsx
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from "react";
+
+export function SparkSchedule() {
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+    if (!mounted) {
+        return <div style={{height: "calc(100vh - 69px)", width: "100vw"}}/>
+    }
+    return (
+        <iframe title={"spark schedule"}
+                src={"https://purdue-ecess.github.io/spark-website/interactive-schedule/"}
+                style={{height: "calc(100vh - 69px)", width: "100vw"}}/>
+    );
+}
