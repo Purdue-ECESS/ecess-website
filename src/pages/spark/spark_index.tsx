eaaf83c13ddef0152580cffa003aeb69c58df0e9
Matthew Wen <mattwen2018@gmail.com>
2021-12-09 06:40:00 -0500

    make sure that div is loaded first, then iframe

--- src/pages/spark/spark_index.tsx
+++ src/pages/spark/spark_index.tsx
@@ -1,13 +1,15 @@
 import React, {useEffect, useState} from "react";
 
 export function SparkIndex() {
+    const [init, setInit] = useState(false);
     const [mounted, setMounted] = useState(false);
 
     useEffect(() => {
         setMounted(true);
     }, []);
 
-    if (!mounted) {
+    if (!init && !mounted) {
+        setInit(true);
         return (
             <div
                 style={{height: "calc(100vh - 69px)", width: "100%"}}/>
