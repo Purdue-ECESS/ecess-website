1e44a8ebfcde5f38f3e284fad0aae658ab446f7e
Matthew Wen <mattwen2018@gmail.com>
2022-01-13 13:18:11 -0500

    started the dashboard, have a lot of work to get the first part working

--- src/data/data_people.tsx
+++ src/data/data_people.tsx
@@ -1,25 +1,52 @@
-import {collection, query, where, getFirestore, Firestore,  getDocs, orderBy} from "firebase/firestore";
+import {collection, query, where, getFirestore, Firestore, getDocs, orderBy, doc, getDoc} from "firebase/firestore";
 
 import {MyFb} from "./data_fb";
 
-const ECESS_MAP = new Map();
+const ECESS_MAP_BY_NAME = new Map();
+const ECESS_MAP_UID = new Map();
 const ORG_REQUESTED = new Set();
 MyFb.loadFb();
 const db: Firestore = getFirestore();
-export async function getPictureUrl(name) {
-    let member = undefined;
-    if (ECESS_MAP.has(name)) {
-        member = ECESS_MAP.get(name);
+
+export async function getPersonByUid(uid) {
+    let member: any = {};
+    if (ECESS_MAP_UID.has(uid)){
+        member = ECESS_MAP_UID.get(uid);
+    }
+    else {
+        const user = await getDoc(doc(collection(db, "users"), uid));
+        member = user.data();
+        ECESS_MAP_UID.set(uid, member);
+        if (member && member?.name) {
+            ECESS_MAP_BY_NAME.set(member.name, member);
+        }
+    }
+    return member;
+}
+
+export async function getPersonByName(name) {
+    let member;
+    if (ECESS_MAP_BY_NAME.has(name)) {
+        member = ECESS_MAP_BY_NAME.get(name);
     }
     else {
         const q = query(collection(db, "users"), where("name", "==", name));
+        let id = undefined;
         (await getDocs(q)).forEach((item) => {
             member = item.data();
+            id = item.id;
         });
         if (member) {
-            ECESS_MAP.set(member.name, member);
+            ECESS_MAP_BY_NAME.set(member.name, member);
+            ECESS_MAP_UID.set(id, member);
         }
     }
+    return member;
+}
+
+
+export async function getPictureUrl(name) {
+    let member = await getPersonByName(name);
     return member ? member.picture: undefined;
 }
 export async function getMembersFromOrganization(organization, retired=false) {
@@ -29,13 +56,14 @@ export async function getMembersFromOrganization(organization, retired=false) {
         const q = query(collection(db, `users`), orderBy(`ecess_organization.${organization}`, "asc"));
         (await getDocs(q)).forEach((item) => {
             const data = item.data();
-            if (!ECESS_MAP.has(data.name)) {
-                ECESS_MAP.set(data.name, data);
+            if (!ECESS_MAP_BY_NAME.has(data.name)) {
+                ECESS_MAP_BY_NAME.set(data.name, data);
+                ECESS_MAP_UID.set(item.id, data);
             }
         })
     }
     let heads = [];
-    ECESS_MAP.forEach((item) => {
+    ECESS_MAP_BY_NAME.forEach((item) => {
         if (item.ecess_organization) {
             if (organization in item.ecess_organization) {
                 if ( (organization.retired && retired) || !(retired || organization.retired)) {
--- src/pages/dashboard/dashboard_index.tsx
+++ src/pages/dashboard/dashboard_index.tsx
@@ -1,9 +1,278 @@
-import {Typography} from "@material-ui/core";
+import {
+    Button,
+    Card,
+    CardContent,
+    Dialog, DialogActions,
+    DialogContent, DialogContentText,
+    DialogTitle,
+    Divider, Menu, MenuItem,
+    Slide, TextField,
+    Typography
+} from "@material-ui/core";
+import {useEffect, useState} from "react";
+import {MyFb} from "src/data/data_fb";
+import {getPersonByUid} from "src/data/data_people";
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import * as React from 'react';
+import {TransitionProps} from "@material-ui/core/transitions";
+
+const Transition = React.forwardRef(function Transition(
+    props: TransitionProps & {
+        children: React.ReactElement<any, any>;
+    },
+    ref: React.Ref<unknown>,
+) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+
+function BasicOptionSelection({label, selections, onSelect}) {
+    const [newLabel, setNewLabel] = useState(label);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = (option) => {
+        setNewLabel(option);
+        onSelect(option)
+        setAnchorEl(null);
+    };
+    return (
+        <>
+            <Typography>Click Save When You Are Done</Typography>
+            <Button
+                variant={"contained"}
+                style={{width: "100%"}}
+                id="basic-button"
+                aria-controls={open ? 'basic-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+            >
+                {newLabel}
+            </Button>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={() => setAnchorEl(null)}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                }}
+            >
+                <>
+                    {
+                        selections.map((item) => {
+                            return (
+                                <MenuItem
+                                    selected={item === newLabel}
+                                    onClick={() => handleClose(item)}
+                                >
+                                    {item}
+                                </MenuItem>
+                            );
+                        })
+                    }
+                </>
+            </Menu>
+        </>
+    )
+}
+
+function AdvancedOptionSelectionAndText({label: month, selections, onSelect}) {
+    let actualMonth = undefined;
+    let actualYear = undefined;
+    selections.forEach((item) => {
+        if (month.startsWith(item)) {
+            actualMonth = item;
+            actualYear = month.substr(item.length + 1);
+        }
+    })
+    const [newLabel, setNewLabel] = useState(actualMonth);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = (option) => {
+        setNewLabel(option);
+        onSelect(option)
+        setAnchorEl(null);
+    };
+    return (
+        <>
+            <Typography>Click Save When You Are Done</Typography>
+            <div>
+                <div style={{display: "flex"}}>
+                    <Button
+                        variant={"contained"}
+                        style={{margin: "0 5px", flex: 1}}
+                        id="basic-button"
+                        aria-controls={open ? 'basic-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick}
+                    >
+                        {newLabel}
+                    </Button>
+                    {newLabel !== "None" &&
+                        <TextField defaultValue={actualYear} />
+                    }
+                </div>
+                <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={() => setAnchorEl(null)}
+                    MenuListProps={{
+                        'aria-labelledby': 'basic-button',
+                    }}
+                >
+                    <>
+                        {
+                            selections.map((item) => {
+                                return (
+                                    <MenuItem
+                                        selected={item === newLabel}
+                                        onClick={() => handleClose(item)}
+                                    >
+                                        {item}
+                                    </MenuItem>
+                                );
+                            })
+                        }
+                    </>
+                </Menu>
+            </div>
+        </>
+    )
+}
+
+
+function OptionName({option, value, insideMessage}) {
+    const [open, setOpen] = React.useState(false);
+    return (
+        <>
+            <Dialog
+                open={open}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={() => {
+                    setOpen(false);
+                }}
+                aria-describedby="alert-dialog-slide-description"
+            >
+                <DialogTitle>{`Set New ${option} ${option === "Graduation" ? "Date": ""}`}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-slide-description">
+                        {insideMessage}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpen(false)}>Cancel</Button>
+                    <Button onClick={() => {
+                        setOpen(false);
+                    }}>Save</Button>
+                </DialogActions>
+            </Dialog>
+            <div>
+                <Button
+                    onClick={() => {
+                        setOpen(true);
+                    }}
+                    style={{display: "flex", flexFlow: "row", padding: 10, width: '100%'}}>
+                    <div>{option}</div>
+                    <div style={{flex: 1, textAlign: "end"}}>{value || `No ${option}`}</div>
+                    <ChevronRightIcon/>
+                </Button>
+                <Divider />
+            </div>
+        </>
+)
+}
 
 export function DashboardIndex({user}) {
+    MyFb.loadFb();
+    const [userData, setUserData] = useState(undefined);
+
+
+    useEffect(() => {
+        if (userData === undefined) {
+            getPersonByUid(user.uid).then(response => {
+                setUserData(response);
+            })
+        }
+    }, [userData, user]);
+
     return (
-        <div>
-            <Typography>Dashboard Screen</Typography>
+        <div style={{maxWidth: 1080, margin: "0 auto"}}>
+            {userData &&
+                <>
+                    <Typography variant={"h5"} style={{textAlign: "center", margin: 10}}>Welcome {userData.name}</Typography>
+                    <Card>
+                        <CardContent>
+                            <div>
+                                <Typography variant={"h6"} style={{padding: 5}}>Basic Information</Typography>
+                                <OptionName
+                                    insideMessage={(
+                                        <>
+                                            <Typography>Click Save When You Are Done</Typography>
+                                            <TextField id="filled-basic" label={"Name"} variant="filled" style={{width: "100%"}}/>
+                                        </>
+                                    )}
+                                    option={"Name"}
+                                    value={user.displayName}
+                                />
+                                <OptionName
+                                    insideMessage={(
+                                        <>
+                                            <Typography>Click Save When You Are Done</Typography>
+                                            <TextField id="filled-basic" label={"Email"} variant="filled" style={{width: "100%"}}/>
+                                        </>
+                                    )}
+                                    option={"Email"}
+                                    value={user.email}
+                                />
+                                <OptionName
+                                    insideMessage={
+                                        (
+                                            <AdvancedOptionSelectionAndText
+                                            label={userData.graduation || "Not Set"}
+                                            selections={["None", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]}
+                                            onSelect={ () => {} }
+                                        />)
+                                    }
+                                    option={"Graduation"}
+                                    value={userData.graduation}
+                                />
+                                <OptionName
+                                    insideMessage={(
+                                        <>
+                                            <Typography>Click Save When You Are Done</Typography>
+                                            <TextField id="filled-basic" label={"Hometown"} variant="filled" style={{width: "100%"}}/>
+                                        </>
+                                    )}
+                                    option={"Hometown"}
+                                    value={userData.hometown}
+                                />
+                                <OptionName
+                                    insideMessage={(
+                                        <BasicOptionSelection
+                                            label={userData.major}
+                                            onSelect={(item) => console.log(item)}
+                                            selections={["Computer Engineering", "Electrical Engineering"]}
+                                        />
+                                    )}
+                                    option={"Major"}
+                                    value={userData.major}
+                                />
+                            </div>
+
+                        </CardContent>
+                    </Card>
+                </>
+            }
         </div>
     );
 }
