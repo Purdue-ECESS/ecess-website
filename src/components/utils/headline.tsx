3add79d006b2a18d5269320eac585269a2f3b1e4
Matthew Wen <mattwen2018@gmail.com>
2022-02-21 10:11:47 -0500

    Finished Adding the Micro Event

---
+++ src/components/utils/headline.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { useSpring, animated } from "react-spring";
+import "src/styles/bootstrap_navbar.sass";
+import {Button, Typography} from "@mui/material";
+
+// https://stackoverflow.com/questions/56685305/how-to-create-moving-text-from-right-to-left-using-react-spring
+export function Headline({link, text}) {
+
+    const [key, setKey] = useState(1);
+
+    const scrolling = useSpring({
+        from: { transform: "translate(60%,0)" },
+        to: { transform: "translate(-60%,0)" },
+        config: { duration: 20000 },
+        reset: true,
+        //reverse: key % 2 == 0,
+        onRest: () => {
+            setKey(key + 1);
+        }
+    });
+
+    return (
+        <Button
+            onClick={() => {
+                window.location.href = link;
+            }}
+            className={"headline"}
+            key={key}>
+            <animated.div style={scrolling}>
+                <Typography style={{padding: 2}} variant={"subtitle2"}>
+                    {text}
+                </Typography>
+            </animated.div>
+        </Button>
+    );
+}
